1b9b3fe6c6f98e435170702ce765e9087caab8ab
ranvirdhanak <72129904+ranvirdhanak@users.noreply.github.com>
2021-10-03 15:49:14 +0530

    Update p107.py

--- p107.py
+++ p107.py
@@ -4,16 +4,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 df = pd.read_csv('data107p.csv')
-student_df = df.loc[df['student_id']=="TRL_xsl"]
-print(student_df.groupby("level")['attempt'].mean())
-fig = go.Figure(go.Bar(
-    x = student_df.groupby("level")['attempt'].mean(),
-    y = ['level 1','level 2','level 3','level 4'],
-    orientation = 'h'
-))
-
-df = pd.read_csv("data107p.csv")
-fig = px.scatter(df,x = "attempt",y = "level",color = "student_id")
-fig.show()
-
+mean = df.groupby(["student_id","level"],as_index = False)['attempt'].mean()
+fig = px.scatter(mean,x = "student_id",y = "level",color = "attempt",size = "attempt")
 fig.show()
